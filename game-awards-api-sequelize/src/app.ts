8598b8631e21b94bf0cea20daa1cfa74b23f0e67
Adriel Santana <diel_san@hotmail.com>
2023-02-20 22:10:44 -0300

    criando estrutura para iniciar API usando express e sequelize

---
+++ game-awards-api-sequelize/src/app.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+const app = express();
+
+import { router } from "./routes";
+
+app.use(express.json());
+
+app.use(router);
+
+const PORT =  8080;
+
+app.listen(PORT, () => {
+  console.log("Started");
+});
