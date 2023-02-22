f7117d15fb1c61413976414d788520e1dea71439
Adriel Santana <diel_san@hotmail.com>
2023-02-21 23:06:17 -0300

    alterando rotas para /api

--- game-awards-api-sequelize/src/routes.ts
+++ game-awards-api-sequelize/src/routes.ts
@@ -4,22 +4,22 @@ import { gameController } from "./controllers/gameController";
 
 const router = express.Router();
 
-router.get("/categories", categoryController.index);
-router.get("/categories/:id", categoryController.show);
-router.post("/categories", categoryController.save);
-router.put("/categories/:id", categoryController.update);
-router.delete("/categories/:id", categoryController.delete);
-router.get("/categories/:categoryId/winner", categoryController.getWinner);
+router.get("/api/categories", categoryController.index);
+router.get("/api/categories/:id", categoryController.show);
+router.post("/api/categories", categoryController.save);
+router.put("/api/categories/:id", categoryController.update);
+router.delete("/api/categories/:id", categoryController.delete);
+router.get("/api/categories/:categoryId/winner", categoryController.getWinner);
 
-router.post("/categories/:categoryId/:gameId", categoryController.addGame);
-router.delete("/categories/:categoryId/:gameId", categoryController.removeGame);
-router.post("/categories/:categoryId/:gameId/vote", categoryController.addVote);
-router.get("/categories/:categoryId/:gameId", categoryController.getGame);
+router.post("/api/categories/:categoryId/:gameId", categoryController.addGame);
+router.delete("/api/categories/:categoryId/:gameId", categoryController.removeGame);
+router.post("/api/categories/:categoryId/:gameId/vote", categoryController.addVote);
+router.get("/api/categories/:categoryId/:gameId", categoryController.getGame);
 
-router.get("/games", gameController.index);
-router.get("/games/:id", gameController.show);
-router.post("/games", gameController.save);
-router.put("/games/:id", gameController.update);
-router.delete("/games/:id", gameController.delete);
+router.get("/api/games", gameController.index);
+router.get("/api/games/:id", gameController.show);
+router.post("/api/games", gameController.save);
+router.put("/api/games/:id", gameController.update);
+router.delete("/api/games/:id", gameController.delete);
 
 export { router };
